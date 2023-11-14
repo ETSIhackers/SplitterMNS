154fcd371946f006e3152cc796017025b1db057c
GeorgeSoult <george.soultanidis@gmail.com>
2023-11-14 20:11:54 +0000

    George Second amplitude update

--- python/split_to_respiratory_gates.py
+++ python/split_to_respiratory_gates.py
@@ -1,3 +1,12 @@
+# date: 2023-11-14
+# Author: Geprgios Soultanidis
+# version: 0.01
+
+# Usage: python split_to_respiratory_gates.py <input_file> <number_of_gates> <minimum_respiratory_amplitude> <maximum_respiratory_amplitude>
+# Amplitude based respiratory gating, where the user defines the number of gates, the minimum and maximum respiratory amplitude
+# The script reads the physio data from a csv file and assigns a gate to each event based on the respiratory amplitude
+# TODO the script will be updated to read the physio data from the raw data file, fully in sinc with the packet time stamps
+
 # Currently hard-wire location of generated files. This will need to change!
 import sys
 
@@ -6,7 +15,20 @@ import math
 import numpy as np
 import prd
 
-# open a csv file and read time stamps (first column) and respiratory_amplitude (second column)
+
+# function to open a csv file and read time stamps (first column) and respiratory_amplitude (second column) and return them into two arrays (time_stamps, respiratory_amplitude)
+# TODO: make this obsolete by reading the physio data from the raw data file
+def read_csv_file(csv_file: str):
+    time_stamps = []
+    respiratory_amplitude = []
+    with open(csv_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            time_stamps.append(float(line.split(",")[0]))
+            respiratory_amplitude.append(float(line.split(",")[1]))
+    return np.array(time_stamps), np.array(respiratory_amplitude)
 
 
 def asign_gate(
@@ -50,4 +72,31 @@ if __name__ == "__main__":
         minimum_respiratory_amplitude,
         maximum_respiratory_amplitude,
     )
-    print("this goes to this gate:", all_events)
+    # load the respirtatory signal TODO this part will be replaced with the in-packet physio data
+    time_stamps, resp_amplitude = read_csv_file("physio/resp_sino.csv")
+    # create acounter for the time stamps, where it starts with 0. TODO, this part will change, if physio is in-packet
+    time_stamp_counter = 0
+    # get all the time blocks and for each one, get the events
+    for time_block in reader.read_time_blocks():
+        # for now, we just assume that the first time block has the same time stamp with the first physio record
+        time_stamp = (
+            time_block.id * 0.001
+        )  # the id are in msec and we convert them in seconds
+        # compare the time stamp of the time block with the physio time stamp at the time_stamp_counter
+        if time_stamp >= time_stamps[time_stamp_counter + 1]:
+            # if the time stamp of the time block is bigger than the physio time stamp, increase the time_stamp_counter by 1
+            time_stamp_counter += 1
+        gate_indicator = asign_gate(
+            resp_amplitude[time_stamp_counter],
+            number_of_gates,
+            minimum_respiratory_amplitude,
+            maximum_respiratory_amplitude,
+        )
+        print(
+            "time stamp is: ",
+            time_stamp,
+            " the amplitude is:",
+            resp_amplitude[time_stamp_counter],
+            "gate is: ",
+            gate_indicator,
+        )
