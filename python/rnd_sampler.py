d7df63a796c96f4c824ca0707a8d20a5d32cc409
Maxime Toussaint <maxime.toussaint@usherbrooke.ca>
2023-11-14 01:07:15 +0000

    First implementation of splitter code, time block working. Event wise WIP

--- python/rnd_sampler.py
+++ python/rnd_sampler.py
@@ -12,13 +12,15 @@ TODO:
 		- Singles rates is not ajusted
 	Feature:
 		- Quicker algo
+		- with thingy
 '''
 
 #########################################################################################
 # Modules
 #########################################################################################
-import sys
 import argparse
+import random
+import sys
 import prd
 
 
@@ -30,21 +32,27 @@ def parserCreator():
 
 	##################################################
 	# Basic
-	parser.add_argument('-acqFile', action='store', type=str, required=True,
+	parser.add_argument('--acqFile', action='store', type=str, required=True,
 	                    dest='acq',
 	                    help='File dsa .')
-	parser.add_argument('-retentionFrac', action='store', type=float, required=True,
-	                    dest='ratioDrop',
+	parser.add_argument('-r', '--retentionFrac', action='store', type=float, required=True,
+	                    dest='retFrac',
 	                    help='The (expected) fraction of retend events.')
-	parser.add_argument('-randoMethod', action='store', choices=["event", "timeBlock"],
+	parser.add_argument('--randoMethod', action='store', choices=["event", "timeBlock"],
 	                    default="timeBlock", dest='randoMethod',
 	                    help='The method used for retention of events.')
-	parser.add_argument('-outputFile', action='store', type=str,
-					default=None, dest='oFile',
-					help='The method used for retention of events.')
+	parser.add_argument('--outputFile', action='store', type=str,
+	                    default=None, dest='oFile',
+	                    help='The method used for retention of events.')
 
 	##################################################
-	# Basic
+	# Feature
+	parser.add_argument('-seed', action='store', type=int,
+					default=None, dest='seed',
+					help='Defines the seed used for retention method.')
+	parser.add_argument('-verbose', action='store', type=int,
+					default=0, dest='verbose',
+					help='Level of verbosity.')
 
 	return parser.parse_args()
 
@@ -55,3 +63,44 @@ def parserCreator():
 if __name__ == "__main__":
 	# do stuff
 	args = parserCreator()
+
+	if args.seed is not None:
+		random.seed(args.seed)
+
+	if args.oFile is None:
+		writerIO = sys.stdout.buffer
+	else:
+		writerIO = args.oFile
+
+	with prd.BinaryPrdExperimentWriter(writerIO) as writer:
+
+		reader = prd.BinaryPrdExperimentReader(args.acq)
+		header = reader.read_header()
+		writer.write_header(header)
+
+		nbTimeBlock = 0
+		nbTimeBlockKept = 0
+		nbPrompt = 0
+		for tbID, time_block in enumerate(reader.read_time_blocks()):
+			nbTimeBlock += 1
+			if args.randoMethod == "timeBlock":
+				# nbPrompt += len(time_block.prompt_event)
+				if random.random() < args.retFrac:
+					nbTimeBlockKept += 1
+					print(f"Current time block {tbID}")
+					tmp = (prd.TimeBlock(id=time_block.id, prompt_events=time_block.prompt_events), )
+					writer.write_time_blocks(tmp)
+			elif args.randoMethod == "event":
+				dsa = 0
+				# for event in time_block.prompt_events:
+				# 	dsa
+		print(f"Number of time block {nbTimeBlock}")
+
+		if nbTimeBlockKept == 0:
+			print("Warning: No event/time block were preserved")
+			tmp = (prd.TimeBlock(id=0, prompt_events=[]), )
+			writer.write_time_blocks(tmp)
+
+
+# Expected: 16,722,947
+# Current:  16,722,946
