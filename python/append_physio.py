aa959eb61d9fa7c0a9843f365fb2cda85d3c82f0
George Soultanidis <36078902+GeorgeSoult@users.noreply.github.com>
2025-07-22 15:16:45 -0400

    Make it possible to append physio data to petsird lm files

---
+++ python/append_physio.py
@@ -0,0 +1,89 @@
+# date: 2025-07-17
+# Author: Georgios Soultanidis
+# Affiliation: BMEII, Icahn School of Medicine at Mount Sinai
+# version: 0.2
+
+# The functionality of this script is to append physiological signals (e.g., ECG) to a PETSIRD file.
+# you have to provide a PETSIRD file and a CSV file with two columns: [start_time_ms, value].
+# The script will read the CSV file and create an ExternalSignalTimeBlock for each row,
+# which will be appended to the PETSIRD file.
+# The output will be a new PETSIRD file with the appended physiological signals.
+
+# TODO: implement a way to select the type of physiological signal (e.g., ECG, respiratory, triggers, etc.)
+
+#the way to run this script is:
+# python append_physio.py -i input.petsird -o output.petsird -p physio.csv
+# where input.petsird is the input PETSIRD file, output.petsird is the output PETSIRD file,
+# and physio.csv is the CSV file with the physiological signals
+
+
+import petsird
+import pandas as pd
+import argparse
+
+
+# Define input and output files
+def parserCreator():
+    parser = argparse.ArgumentParser(
+        prog="append_physio",
+        description="Append physiological signals to a PETSIRD file",
+    )
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="Input PETSIRD file"
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, required=True, help="Output PETSIRD file"
+    )
+    parser.add_argument(
+        "-p",
+        "--physio",
+        type=str,
+        required=True,
+        help="Physiological data file (CSV with start_time_ms,value)",
+    )
+    return parser.parse_args()
+
+
+args = parserCreator()
+
+# Read CSV with two columns: [start_time_ms, value]
+df = pd.read_csv(args.physio, header=None)
+starts_ms = df.iloc[:, 0].astype(int).values
+values = df.iloc[:, 1].astype(int).values
+
+ecg_id = 1  # must be unique and consistent with your data
+
+# Open reader and writer
+with petsird.BinaryPETSIRDReader(args.input) as reader, open(
+    args.output, "wb"
+) as out_file, petsird.BinaryPETSIRDWriter(out_file) as writer:
+
+    # Read and modify header
+    header = reader.read_header()
+
+    if not any(sig.id == ecg_id for sig in header.exam.external_signals):
+        ecg_signal = petsird.ExternalSignal(
+            type=petsird.ExternalSignalTypeEnum.ECG_TRIGGER,
+            description="ECG signal from CSV (2-column format)",
+            id=ecg_id,
+        )
+        header.exam.external_signals.append(ecg_signal)
+
+    # Write modified header
+    writer.write_header(header)
+
+    # Copy all time blocks
+    for block in reader.read_time_blocks():
+        writer.write_time_blocks((block,))
+
+    # Inject one ExternalSignalTimeBlock per row
+    for t_ms, val in zip(starts_ms, values):
+        time_interval = petsird.TimeInterval(start=int(t_ms), stop=int(t_ms + 1))
+        ecg_block = petsird.ExternalSignalTimeBlock(
+            time_interval=time_interval,
+            signal_id=ecg_id,
+            signal_values=[val],
+        )
+        writer.write_time_blocks(
+            (petsird.TimeBlock.ExternalSignalTimeBlock(ecg_block),)
+        )
